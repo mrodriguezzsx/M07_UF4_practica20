47789c1d79eae9ca31701ec9ba39c7b76603ec57
adriabalmes <aberruez@jaumebalmes.net>
2023-04-27 19:13:01 +0200

    version 1

--- tenda_grupG/tenda_grupG/settings.py
+++ tenda_grupG/tenda_grupG/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'tenda_grupG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apirest_practica20',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
+
 
 
 # Password validation
